a5e083a9b3814e25cd16dc64dee64e43e1882c9d
Mridha Imran Kabir <44534771+imrankabir02@users.noreply.github.com>
2024-10-16 10:17:22 +0600

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,25 +2,29 @@ import { PROJECTS } from "../constants"
 
 const Projects = () => {
     return (
-        <div className="border-b border-neutral-900 pb-4">
-            <h1 className="my-20 text-center text-4xl">
+        <div className="pb-12 text-white border-b border-neutral-800 lg:pb-20 bg-gradient-to-r from-slate-925 to-gray-900">
+            <h1 className="my-20 text-4xl font-semibold text-center text-white">
                 Projects
             </h1>
-            <div>
+            <div className="transition-all duration-500 ease-in-out transform bg-slate-910 hover:bg-slate-910 hover:scale-105">
                 {PROJECTS.map((project, index) => (
-                    <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+                    <div key={index} className="flex flex-wrap mb-8 lg:justify-center">
                         <div className="w-full lg:w-1/4">
-                            <img src={project.image} width={150} height={150} alt={project.title} />
+                            <a href="https://book-store-mern-psi.vercel.app">
+                                <img src={project.image} width={300} height={150} alt={project.title} />
+                            </a>
                         </div>
                         <div className="w-full max-w-xl lg:w-3/4">
-                            <h6 className="mb-2 text-neutral-400">
-                                {project.title}
-                            </h6>
+                            <a href="https://github.com/imrankabir02/book-store-MERN.git">
+                                <h6 className="mb-2 text-2xl font-semibold text-neutral-400 hover:text-blue-400">
+                                    {project.title}
+                                </h6>
+                            </a>
                             <p className="mb-4 text-neutral-400">
                                 {project.description}
                             </p>
                             {project.technologies.map((tech, idx) => (
-                                <span key={idx} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-600">
+                                <span key={idx} className="gap-3 px-3 py-1 mt-2 text-sm font-medium text-white bg-indigo-500 rounded-full shadow hover:bg-indigo-400">
                                     {tech}
                                 </span>
                             ))}
