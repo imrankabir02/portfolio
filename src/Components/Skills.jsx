fbe0a46112b9c7b7e517ccb377b5614d102c4e7e
imrankabir02 <imrankabir325@gmail.com>
2024-12-24 11:49:15 +0600

    code updated

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -14,13 +14,15 @@ const Skills = () => {
                 <div className="flex flex-col items-center justify-center">
                     <h2 className="text-xl font-semibold text-center">Languages</h2>
                     <div className="flex flex-wrap justify-center gap-6 mt-4">
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><DiJsBadge className="text-6xl text-yellow-500" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><DiPhp className="text-6xl text-purple-700" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600">
+                        <div className="skillset">
+                            <DiJsBadge className="text-6xl text-yellow-500" />
+                        </div>
+                        <div className="skillset"><DiPhp className="text-6xl text-purple-700" /></div>
+                        <div className="skillset">
                             <FaPython className="text-6xl text-green-500" />
                         </div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><FaJava className="text-6xl text-orange-600" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600">
+                        <div className="skillset"><FaJava className="text-6xl text-orange-600" /></div>
+                        <div className="skillset">
                             <SiC className="text-6xl text-blue-500" />
                         </div>
                     </div>
@@ -30,11 +32,11 @@ const Skills = () => {
                 <div className="flex flex-col items-center justify-center">
                     <h2 className="text-xl font-semibold text-center">Libraries and Frameworks</h2>
                     <div className="flex flex-wrap justify-center gap-6 mt-4">
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><FaLaravel className="text-6xl text-red-600" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><SiDjango className="text-6xl text-green-700" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><DiReact className="text-6xl text-cyan-400" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><SiTailwindcss className="text-6xl text-teal-400" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><SiBootstrap className="text-6xl text-purple-500" /></div>
+                        <div className="skillset"><FaLaravel className="text-6xl text-red-600" /></div>
+                        <div className="skillset"><SiDjango className="text-6xl text-green-700" /></div>
+                        <div className="skillset"><DiReact className="text-6xl text-cyan-400" /></div>
+                        <div className="skillset"><SiTailwindcss className="text-6xl text-teal-400" /></div>
+                        <div className="skillset"><SiBootstrap className="text-6xl text-purple-500" /></div>
                     </div>
                 </div>
 
@@ -42,10 +44,10 @@ const Skills = () => {
                 <div className="flex flex-col items-center justify-center">
                     <h2 className="text-xl font-semibold text-center">Technologies and Databases</h2>
                     <div className="flex flex-wrap justify-center gap-6 mt-4">
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><DiHtml5 className="text-6xl text-red-500" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><DiCss3 className="text-6xl text-blue-500" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><DiMysql className="text-6xl text-blue-700" /></div>
-                        {/* <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><SiDocker className="text-6xl text-blue-400" /></div> */}
+                        <div className="skillset"><DiHtml5 className="text-6xl text-red-500" /></div>
+                        <div className="skillset"><DiCss3 className="text-6xl text-blue-500" /></div>
+                        <div className="skillset"><DiMysql className="text-6xl text-blue-700" /></div>
+                        {/* <div className="skillset"><SiDocker className="text-6xl text-blue-400" /></div> */}
                     </div>
                 </div>
 
@@ -53,8 +55,8 @@ const Skills = () => {
                 <div className="flex flex-col items-center justify-center">
                     <h2 className="text-xl font-semibold text-center">Development Tools</h2>
                     <div className="flex flex-wrap justify-center gap-6 mt-4">
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><FaGitAlt className="text-6xl text-orange-600" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><SiVisualstudiocode className="text-6xl text-blue-600" /></div>
+                        <div className="skillset"><FaGitAlt className="text-6xl text-orange-600" /></div>
+                        <div className="skillset"><SiVisualstudiocode className="text-6xl text-blue-600" /></div>
                     </div>
                 </div>
 
@@ -62,9 +64,9 @@ const Skills = () => {
                 <div className="flex flex-col items-center justify-center">
                     <h2 className="text-xl font-semibold text-center">Operating Systems</h2>
                     <div className="flex flex-wrap justify-center gap-6 mt-4">
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><FaWindows className="text-6xl text-blue-600" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><SiUbuntu className="text-6xl text-orange-600" /></div>
-                        <div className="p-4 transition-all duration-500 transform border shadow-lg rounded-xl border-neutral-600 hover:scale-125 ease-in-outhover:bg-slate-600"><FaLinux className="text-6xl text-yellow-500" /></div>
+                        <div className="skillset"><FaWindows className="text-6xl text-blue-600" /></div>
+                        <div className="skillset"><SiUbuntu className="text-6xl text-orange-600" /></div>
+                        <div className="skillset"><FaLinux className="text-6xl text-yellow-500" /></div>
                     </div>
                 </div>
             </div>
